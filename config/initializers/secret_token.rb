cca35e01f8858f2d7f975ebd5704713cff4cde79
ab11w <ab11w@my.fsu.edu>
2013-04-23 20:47:54 -0400

    Fixed crlf issue

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'be53546ad21b0a7c4fd83c46178d4d69f2f5b4fde05727863f9592079dbe091661a5bf1e5f5fb6adaa6bc3071c155454be4d3a76e4ba00ea951b896a90f21755'
